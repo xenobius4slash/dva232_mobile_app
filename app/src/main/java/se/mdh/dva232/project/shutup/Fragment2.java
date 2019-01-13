4e53ffdfe85e4b5c6119f4f8d936dc5cf32bb9ee
alber <alherraez1@gmail.com>
2019-01-13 15:32:22 +0100

    listview code

--- app/src/main/java/se/mdh/dva232/project/shutup/Fragment2.java
+++ app/src/main/java/se/mdh/dva232/project/shutup/Fragment2.java
@@ -6,8 +6,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.sql.Struct;
 
 public class Fragment2 extends Fragment {
+
+    //define variables
+    ListView list;
+
+    //struct that contains the elements of each item
+    /*class elements
+    {
+        public String name;
+        public String duration;
+    }*/
+
+
+    String [] item_elements =
+            {"event 1","11:00 - 12:00"};
+            //{"event 2","12:00 - 13:00"},
+           // {"event 3","13:00 - 14:00"},
+    //};
 
     public Fragment2() {
         // Required empty public constructor
@@ -25,6 +45,7 @@ public class Fragment2 extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         Log.d("LIFECYCLE Fragment2", "onCreate");
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -32,6 +53,13 @@ public class Fragment2 extends Fragment {
         Log.d("LIFECYCLE Fragment2", "onCreateView(...)");
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+        list = rootView.findViewById(R.id.listView_Items);
+
+        Log.d("set adapter", "setting listi view and item");
+
+        list.setAdapter(new ListView_Adapter(getContext(),item_elements));
+
         return rootView;
     }
 
---
+++ app/src/main/java/se/mdh/dva232/project/shutup/ListView_Adapter.java
@@ -0,0 +1,80 @@
+package se.mdh.dva232.project.shutup;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
+
+public class ListView_Adapter extends BaseAdapter{
+
+    private static LayoutInflater inflater = null;
+    Context context;
+    String [] data;
+
+    //Constructor
+    public ListView_Adapter(Context c, String [] d)
+    {
+        Log.d("listview","entering constructor");
+
+        this.context = c;
+        this.data = d;
+
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        Log.d("getView","entering getView function");
+        //Declare variables
+        final View view = inflater.inflate(R.layout.list_item,null);
+
+        TextView name = (TextView) view.findViewById(R.id.item_name);
+        TextView duration = (TextView) view.findViewById(R.id.item_duration);
+        ImageButton button = (ImageButton) view.findViewById(R.id.delete_button);
+
+        //set text
+        name.setText(data[position]);
+        duration.setText(data[position]);
+        button.setImageResource(R.drawable.baseline_delete_black_18dp);
+
+        Log.d("getView","name: "+name+" ;Duration: "+duration);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText( context, "Delete button clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+}
